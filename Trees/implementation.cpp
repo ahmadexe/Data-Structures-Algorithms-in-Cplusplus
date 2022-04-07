5db0733b136e35de0a5f3ee6787f086380137a6c
Ahmad <muahmad710@gmail.com>
2022-04-07 11:56:28 +0500

    Starting trees

---
+++ Trees/implementation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    Node* right;
+    Node* left;
+};
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
