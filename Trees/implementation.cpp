a7056892fafceed163c24ad922eccd28574cd2a2
Ahmad <muahmad710@gmail.com>
2022-04-07 12:09:55 +0500

    Starting trees

--- Trees/implementation.cpp
+++ Trees/implementation.cpp
@@ -4,20 +4,29 @@ using namespace std;
 
 struct Node 
 {
-    int data;
+    char data;
     Node* right;
     Node* left;
 };
-
-
 
 int main(int argc, char const *argv[])
 {
 
     Node* root = new Node;
     Node* left = new Node;    
-    Node* right = new Node;    
+    Node* right = new Node;
 
+    root->data = 'A';
+    root->left = left;
+    root->right = right;
+
+    left->data = 'B';
+    left->left = NULL;
+    left->right = NULL;
+
+    right->data = 'C';
+    right->left = NULL;
+    right->right = NULL;
 
 
     return 0;
