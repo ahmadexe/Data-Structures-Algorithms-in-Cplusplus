727e8ce38b055010bd026320cb031a0b25c3e8a7
Ahmad <muahmad710@gmail.com>
2022-04-11 12:52:57 +0500

    Double Ended Queue

---
+++ Double ended queue/implementation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class DEQueue{
+private:
+    int f = -1;
+    int r = -1;
+public:
+    
+};
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
