97640b9dab7e461606c03dd010c7638d35089094
Ahmad <muahmad710@gmail.com>
2022-04-12 09:07:07 +0500

    BST

--- Trees/binarySearchTree.cpp
+++ Trees/binarySearchTree.cpp
@@ -24,7 +24,13 @@ void insert(int data)
         Node* p = new Node;
         p = root;
         while (p->left != NULL || p->right != NULL){
-
+            if (p->data < data)
+            {
+                p = p->right;
+            }
+            else{
+                p = p->left;
+            }
         }
 
     }
