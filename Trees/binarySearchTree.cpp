e598e893d9933199149a20e26c51b1b4b9c75211
Ahmad <muahmad710@gmail.com>
2022-04-12 09:09:35 +0500

    BST

--- Trees/binarySearchTree.cpp
+++ Trees/binarySearchTree.cpp
@@ -1,46 +1,53 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node 
+struct Node
 {
     char data;
-    Node* right;
-    Node* left;
+    Node *right;
+    Node *left;
 };
 
-Node* root = NULL;
+Node *root = NULL;
 
 void insert(int data)
 {
-    if (!root) {
-        Node* cur = new Node;
-        cur->data = data;
-        cur->left = NULL;
-        cur->right = NULL;
+    Node *cur = new Node;
+    cur->data = data;
+    cur->left = NULL;
+    cur->right = NULL;
+    if (!root)
+    {
         root = cur;
     }
-    else {
-        Node* p = new Node;
+    else
+    {
+        Node *p = new Node;
         p = root;
-        while (p->left != NULL || p->right != NULL){
+        while (p->left != NULL || p->right != NULL)
+        {
             if (p->data < data)
             {
                 p = p->right;
             }
-            else{
+            else
+            {
                 p = p->left;
             }
         }
-
+        if (data > p->data)
+        {
+            p->right = cur;
+        }
+        else{
+            p->left = cur;
+        }
     }
 }
 
 int main(int argc, char const *argv[])
 {
-
-    
-
 
     return 0;
 }
