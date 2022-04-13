cb6b8feae9e15542c8d98b4a074c541bd2b6918b
Ahmad <muahmad710@gmail.com>
2022-04-14 00:59:07 +0500

    Priority Queue

--- Priority Queue/implementation.cpp
+++ Priority Queue/implementation.cpp
@@ -15,12 +15,21 @@ int isFull(Queue* q)
     return (q->front == 0 && q->rear == q->size - 1);
 }
 
-
+int isEmpty(Queue* q)
+{
+    return (q->front == -1 && q->rear == - 1);
+}
 
 int main(int argc, char const *argv[])
 {
 
-    
+    Queue *q = new Queue;
+    q->front = -1;
+    q->rear = -1;
+    q->size = 5;
+    q->arr = (int*)malloc(sizeof(int) * q->size);
+    cout<<isEmpty(q)<<endl;
+    cout<<isFull(q)<<endl;
 
 
     return 0;
