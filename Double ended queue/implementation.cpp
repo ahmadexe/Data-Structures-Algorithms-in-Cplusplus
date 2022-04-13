d7a6363693316ddad59ad21ea9f483fc91da59cb
Ahmad <muahmad710@gmail.com>
2022-04-13 09:10:37 +0500

    More on queues

--- Double ended queue/implementation.cpp
+++ Double ended queue/implementation.cpp
@@ -2,6 +2,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Deq{
+
+int front = -1;
+int rear = 0;
+int size;
+int arr[100];
+
+public:
+    
+
+};
 
 int main(int argc, char const *argv[])
 {
