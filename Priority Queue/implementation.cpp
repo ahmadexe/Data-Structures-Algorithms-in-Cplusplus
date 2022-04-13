47599f88d9de6b76ee04bdd2fb3d027195f56f34
Ahmad <muahmad710@gmail.com>
2022-04-14 01:12:36 +0500

    Priority Queue

--- Priority Queue/implementation.cpp
+++ Priority Queue/implementation.cpp
@@ -20,6 +20,37 @@ int isEmpty(Queue *q)
     return (q->front == -1 && q->rear == -1);
 }
 
+void enQueue(Queue* q, int value)
+{
+    if (isEmpty(q))
+    {
+        q->front++;
+        q->rear++;
+        q->arr[q->front] = value;
+    }
+    else
+    {
+        int i = q->rear;
+        int j = i+1;
+        while (i != -1 && q->arr[j] <= value)
+        {
+            i--;
+            j--;
+        }
+        q->arr[j] = value;
+        q->rear++;
+    }
+}
+
+void printArray(Queue* q)
+{
+    for (int i = q->front; i <= q->rear; i++)
+    {
+        cout<<q->arr[i]<<" "<<endl;
+    }
+    cout<<endl;
+}
+
 int main(int argc, char const *argv[])
 {
 
@@ -28,8 +59,11 @@ int main(int argc, char const *argv[])
     q->rear = -1;
     q->size = 5;
     q->arr = (int *)malloc(sizeof(int) * q->size);
-    cout << isEmpty(q) << endl;
-    cout << isFull(q) << endl;
+    enQueue(q,30);
+    enQueue(q,20);
+    enQueue(q,50);
+    enQueue(q,10);
+    printArray(q);
 
     return 0;
 }
