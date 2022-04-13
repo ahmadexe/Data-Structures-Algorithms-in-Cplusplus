30d29b2a95eb58d67ea11ec8f0f0f70b72f58d8a
Ahmad <muahmad710@gmail.com>
2022-04-13 09:17:02 +0500

    More on queues

--- Double ended queue/implementation.cpp
+++ Double ended queue/implementation.cpp
@@ -4,13 +4,35 @@ using namespace std;
 
 class Deq{
 
-int front = -1;
-int rear = 0;
+int front;
+int rear;
 int size;
 int arr[100];
 
 public:
-    
+    Deq(int s)
+    {
+        rear = 0;
+        front = -1;
+        size = s;
+    }
+    int isFull()
+    {
+        if (front == 0 && rear == size - 1 || front == rear + 1)
+        {
+            return 1; 
+        }
+        return 0;
+    }
+
+    int isEmpty()
+    {
+        if (front = -1 || rear == -1)
+        {
+            return 1;
+        }
+        return 0;
+    }
 
 };
 
