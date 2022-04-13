1fefd9e2dc826fd4110352d3ffe3a0d5a30de955
Ahmad <muahmad710@gmail.com>
2022-04-13 08:44:47 +0500

    More on queues

--- Double ended queue/implementation.cpp
+++ Double ended queue/implementation.cpp
@@ -6,15 +6,20 @@ class DEQueue{
 public:
     int f = -1;
     int r = -1;
+    int size = 0;
+    DEQueue(int size)
+    {
+        this->size = size;
+    }
     int isEmpty(){
-        if (this->f == this->r){
+        if (this->f == this->r - 1){
             return 1;
         }
         return 0;
     }
     int isFull()
     {
-        if (this->f == this->r)
+        if (this->f = 0 && this->r == this->size - 1)
         {
             return 1;
         }
@@ -22,11 +27,14 @@ public:
     }
 };
 
-
+void enQueueFront(DEQueue* q)
+{
+    
+}
 
 int main(int argc, char const *argv[])
 {
-    DEQueue* q = new DEQueue();
+    DEQueue* q = new DEQueue(5);
     cout<<q->isFull()<<endl;
     cout<<q->isEmpty()<<endl;
 
