ee5704ff43868cb4fbd90c66e61ffbff96cb2ba0
Ahmad <muahmad710@gmail.com>
2022-04-13 09:31:45 +0500

    More on queues

--- Double ended queue/implementation.cpp
+++ Double ended queue/implementation.cpp
@@ -12,13 +12,13 @@ int arr[100];
 public:
     Deq(int s)
     {
-        rear = 0;
+        rear = -1;
         front = -1;
         size = s;
     }
     int isFull()
     {
-        if (front == 0 && rear == size - 1 || front == rear + 1)
+        if (front == 0 && rear == size - 1)
         {
             return 1; 
         }
@@ -32,6 +32,11 @@ public:
             return 1;
         }
         return 0;
+    }
+
+    void insertAtEnd()
+    {
+        
     }
 
 };
