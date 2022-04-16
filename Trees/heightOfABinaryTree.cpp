e88b33c832bf2a760d2b50bb89c07d17b0851d92
Ahmad <muahmad710@gmail.com>
2022-04-16 20:22:08 +0500

    Finding Height of the Tree

--- Trees/heightOfABinaryTree.cpp
+++ Trees/heightOfABinaryTree.cpp
@@ -2,6 +2,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *right;
+    Node *left;
+};
+
+Node* root = NULL;
 
 int main(int argc, char const *argv[])
 {
