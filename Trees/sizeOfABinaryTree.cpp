fcf15e86044265e8b76dcdfa292e28cbfe982bdb
Ahmad <muahmad710@gmail.com>
2022-04-16 20:54:18 +0500

    Size of a Binary Tree

---
+++ Trees/sizeOfABinaryTree.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *right;
+    Node *left;
+};
+
+Node* root = NULL;
+
+
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
