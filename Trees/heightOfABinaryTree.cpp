4fd2654da7a490efb2f4fe3523918caeabfc2fc4
Ahmad <muahmad710@gmail.com>
2022-04-16 20:22:26 +0500

    Finding Height of the Tree

--- Trees/heightOfABinaryTree.cpp
+++ Trees/heightOfABinaryTree.cpp
@@ -11,6 +11,45 @@ struct Node
 
 Node* root = NULL;
 
+
+void insert(int data)
+{
+    Node *cur = new Node;
+    cur->data = data;
+    cur->left = NULL;
+    cur->right = NULL;
+    if (!root)
+    {
+        root = cur;
+    }
+    else
+    {
+        Node *p = new Node;
+        p = root;
+        Node* temp = new Node;
+        while (p != NULL)
+        {
+            if (p->data < data)
+            {
+                temp = p;
+                p = p->right;
+            }
+            else
+            {
+                temp = p;
+                p = p->left;
+            }
+        }
+        p = cur;
+        if (p->data > temp->data){
+            temp->right = p;
+        }
+        else {
+            temp->left = p;
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 
