62a97ba33838df7f8f3c009d55e5a9e0bd7ef062
Ahmad <muahmad710@gmail.com>
2022-04-20 00:59:48 +0500

    More on stacks

--- Stack/postfixToInfix.cpp
+++ Stack/postfixToInfix.cpp
@@ -2,76 +2,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Stack
+class Stack
 {
-    int top;
+public:
+    int top = -1;
+    char* arr;
     int size;
-    string *arr;
+
+    Stack(int size)
+    {
+        this->size = size;
+        this->arr = (char*)malloc(sizeof(char) * this->size);
+    }
+
+    int isEmpty()
+    {
+        if (this->top == -1)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    int isFull()
+    {
+        if (this->top == this->size - 1)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    void push(char data)
+    {
+        if (this->isFull())
+        {
+            cout << "Stack overflow" << endl;
+        }
+        else
+        {
+            this->top++;
+            this->arr[top] = data;
+        }
+    }
+
+    char pop()
+    {
+        if (!this->isEmpty())
+        {
+            int valToReturn = arr[top];
+            this->top--;
+            return valToReturn;
+        }
+        else
+        {
+            cout << "Stack underflow" << endl;
+            return '\0';
+        }
+    }
 };
 
-int isEmpty(Stack *s)
-{
-    return (s->top == -1);
-}
-
-int isFull(Stack *s)
-{
-    return (s->top == s->size - 1);
-}
-
-void push(Stack *s, string val)
-{
-    if (!isFull(s))
-    {
-        s->arr[++s->top] = val;
-    }
-    else
-    {
-        cout << "Stack overflow!" << endl;
-    }
-}
+Stack *s = new Stack(99);
 
 
 
-string convertToInfix(Stack* s,string postfix) {
-    int i = 0;
-    while (i < postfix.size())
-    {
-        if ((postfix[i] >= 'a' && postfix[i] <= 'z') || (postfix[i] >= 'A' && postfix[i] <= 'Z'))
-        {   
-            string toPush = "";
-            toPush += postfix[i];
-            push(s, toPush);
-        }
-        else {
-            string exp = "";
-            string a = pop(s);
-            
-            string b = pop(s);
-            
-            exp = "("+b+postfix[i]+a+")";
-            push(s,exp);
-        }
-        i++;
-    }
-    return pop(s);
-}
-
-string pop(Stack *s)
-{
-    if (!isEmpty(s))
-    {
-       return s->arr[s->top--];
-    }
-    else
-    {
-        cout << "Can't pop Stack underflow" << endl;
-        return "-1";
-    }
-}
 
 int main(int argc, char const *argv[])
 {
-
+    
     return 0;
 }
