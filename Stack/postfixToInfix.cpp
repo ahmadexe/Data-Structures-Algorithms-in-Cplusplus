5928e73612ad0e8f87adaca5fea59a498e29d857
Ahmad <muahmad710@gmail.com>
2022-04-20 00:33:03 +0500

    More on stacks

--- Stack/postfixToInfix.cpp
+++ Stack/postfixToInfix.cpp
@@ -8,6 +8,13 @@ struct Stack{
     int* arr;
 };
 
+int isEmpty(Stack* s){
+    return (s->top == -1);
+}
+
+int isFull(Stack* s) {
+    return (s->top == s->size-1);
+}
 
 
 int main(int argc, char const *argv[])
