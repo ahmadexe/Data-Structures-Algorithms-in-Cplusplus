caafbde0a4b24e71e5e94c42af8f0a3ec953d83c
Ahmad <muahmad710@gmail.com>
2022-04-20 09:02:15 +0500

    Double Linked List Implementation

--- Stack/stackUsingSinglyLL.cpp
+++ Stack/stackUsingSinglyLL.cpp
@@ -2,6 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node 
+{
+    char data;
+    Node* next;
+};
+
+Node* head = NULL;
+Node* tail = NULL;
+
+void insert(char val)
+{
+    
+}
 
 int main(int argc, char const *argv[])
 {
