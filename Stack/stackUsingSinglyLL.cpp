514a0c4501d21bbf59be2a10588d672c874fa8d9
Ahmad <muahmad710@gmail.com>
2022-04-20 09:12:51 +0500

    Signly Linked List Implementataion

--- Stack/stackUsingSinglyLL.cpp
+++ Stack/stackUsingSinglyLL.cpp
@@ -26,19 +26,40 @@ void insert(char val)
         tail = c;
     }   
 }
+string s1 = "";
+string s2 = "";
+void checkPalindrome(Node* p) {
 
-int checkPalindrome(string str, Node* p) {
     if (!p) 
     {
-        
+        return;
+    }
+    else
+    {
+        s1 += p->data;
+        checkPalindrome(p->next);
+        s2 += p->data;
     }
 }
+
+
 
 int main(int argc, char const *argv[])
 {
 
-    
-
+    string s = "civic";
+    for (int i = 0; i < s.size(); i++)
+    {
+        insert(s[i]);
+    }
+    Node* p = head;
+    checkPalindrome(p);
+    if (s1 == s2) {
+        cout<<"palindrome"<<endl;
+    }
+    else{
+        cout<<"NOT";
+    }
 
     return 0;
 }
