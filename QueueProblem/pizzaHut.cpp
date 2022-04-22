9cc40d9da97f4bda6495c8e8567a03ebb75ceaaf
Ahmad <muahmad710@gmail.com>
2022-04-22 13:01:42 +0500

    Starting some problems!

--- QueueProblem/pizzaHut.cpp
+++ QueueProblem/pizzaHut.cpp
@@ -28,13 +28,13 @@ void takeOrder(Queue* q, int oid)
     }
     else if (isEmpty(q))
     {
-        q->f = (q->f + 1) % q->size;
-        q->r = (q->r + 1) % q->size;
+        q->f = 0;
+        q->r = 0;
         q->arr[q->f] = oid;
     }
     else
     {
-        q->r += (q->r + 1) % q->size;
+        q->r = (q->r + 1) % q->size;
         q->arr[q->r] = oid;
     }
 }
@@ -48,14 +48,16 @@ int deliver(Queue* q)
     }
     else
     {
-        int id = q->arr[q->f];
-            q->f = (q->f + 1) % q->size;
         if (q->f != q->r)
         {
+            int id = q->arr[q->f];
+            q->f = (q->f + 1) % q->size;
             return id;
         }
         else
         {
+            int id = q->arr[q->f];
+            q->f = (q->f + 1) % q->size;
             q->f = -1;
             q->r = -1;
             return id;
@@ -65,6 +67,31 @@ int deliver(Queue* q)
 
 int main(int argc, char const *argv[])
 {
-    
+    Queue* q = new Queue;
+    q->f = -1;
+    q->r = -1;
+    q->size = 5;
+    q->arr = (int *)malloc(sizeof(int) * q->size);
+
+    takeOrder(q,10);
+    takeOrder(q,20);
+    takeOrder(q,30);
+    cout<<deliver(q)<<endl;
+    cout<<deliver(q)<<endl;
+    takeOrder(q,40);
+    takeOrder(q,50);
+    takeOrder(q,60);
+    takeOrder(q,70);
+
+    cout<<deliver(q)<<endl;
+    cout<<deliver(q)<<endl;
+    cout<<deliver(q)<<endl;
+    cout<<deliver(q)<<endl;
+    cout<<deliver(q)<<endl;
+    cout<<deliver(q)<<endl;
+
+
+
+
     return 0;
 }
