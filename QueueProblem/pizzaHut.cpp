c7e74a67b9994c23933abcd1bdbe7e823ddc5c15
Ahmad <muahmad710@gmail.com>
2022-04-22 14:38:35 +0500

    Starting some problems!

--- QueueProblem/pizzaHut.cpp
+++ QueueProblem/pizzaHut.cpp
@@ -7,7 +7,7 @@ struct Queue
     int f = -1;
     int r = -1;
     int size;
-    string *arr;
+    int *arr;
 };
 
 int isEmpty(Queue *q)
@@ -20,7 +20,7 @@ int isFull(Queue *q)
     return ((q->r + 1) % q->size == q->f);
 }
 
-void takeOrder(Queue *q, string oid)
+void takeOrder(Queue *q, int oid)
 {
     if (isFull(q))
     {
@@ -39,23 +39,24 @@ void takeOrder(Queue *q, string oid)
     }
 }
 
-string deliver(Queue *q)
+int deliver(Queue *q)
 {
     if (isEmpty(q))
     {
-        return "No pending orders";
+        cout << "No pending orders"<< endl;
+        return -1;
     }
     else
     {
         if (q->f != q->r)
         {
-            string id = q->arr[q->f];
+            int id = q->arr[q->f];
             q->f = (q->f + 1) % q->size;
             return id;
         }
         else
         {
-            string id = q->arr[q->f];
+            int id = q->arr[q->f];
             q->f = (q->f + 1) % q->size;
             q->f = -1;
             q->r = -1;
@@ -71,8 +72,9 @@ int main(int argc, char const *argv[])
     q->r = -1;
     int M;
     cout << "Enter total number of accepted orders" << endl;
+    cin>>M;
     q->size = M;
-    q->arr = (string *)malloc(sizeof(int) * q->size);
+    q->arr = (int *)malloc(sizeof(int) * q->size);
 
     while (1)
     {
@@ -91,9 +93,9 @@ int main(int argc, char const *argv[])
                 cin >> choiceC;
                 if (choiceC == 1)
                 {
-                    string order;
-                    cout << "Enter order " << endl;
-                    getline(cin, order);
+                    int order;
+                    cout << "Enter order id" << endl;
+                    cin>>order;
                     takeOrder(q, order);
                 }
                 else if (choiceC == 2)
