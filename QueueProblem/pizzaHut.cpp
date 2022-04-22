fe17fb08a0f08f3f4175696eb9896e3a1dea8e97
Ahmad <muahmad710@gmail.com>
2022-04-22 12:35:27 +0500

    Starting some problems!

--- QueueProblem/pizzaHut.cpp
+++ QueueProblem/pizzaHut.cpp
@@ -15,6 +15,11 @@ int isEmpty(Queue* q)
     return (q->f == -1 && q->r == -1);
 }
 
+int isFull(Queue * q)
+{
+    return ((q->r + 1) % q->size == q->f);
+}
+
 
 
 int main(int argc, char const *argv[])
