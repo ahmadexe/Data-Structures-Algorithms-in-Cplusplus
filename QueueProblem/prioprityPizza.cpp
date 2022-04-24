57039da8004de2764b6e732e26a0f115bfd3b53e
Ahmad <muahmad710@gmail.com>
2022-04-24 06:46:31 +0500

    Some more problems

--- QueueProblem/prioprityPizza.cpp
+++ QueueProblem/prioprityPizza.cpp
@@ -30,9 +30,6 @@ void enQueue(Queue *q, int value, string ord, vector<string> &order)
         q->rear = 0;
         q->arr[q->front] = value;
         order[q->front] = ord;
-        cout<<q->arr[0]<<endl;
-        cout<<order[0]<<endl;
-        cout<<"---------------------------"<<endl;
     }
     else
     {
@@ -68,13 +65,13 @@ void enQueue(Queue *q, int value, string ord, vector<string> &order)
             flagI = true;
             flagJ = true;
         }
-        cout<<j<<" "<<"adding here "<<value<<endl;
+
         q->arr[j] = value;
-        cout<<q->arr[j]<<endl;
+
         order[j] = ord;
-        cout<<order[j]<<endl;
+
         q->rear = (q->rear + 1) % q->size;
-        cout<<"----------------"<<endl;
+
     }
 }
 
@@ -85,7 +82,7 @@ void deQueueMax(Queue *q, vector<string> order)
         // return -1;
         cout << "NOOOO" << endl;
     }
-    cout << q->arr[q->front] << " order is " << order[q->front] << endl;
+    cout <<"Agr is "<< q->arr[q->front] << " order is " << order[q->front] << endl;
     q->front = (q->front + 1) % q->size;
 }
 
@@ -100,7 +97,7 @@ int main(int argc, char const *argv[])
     cin>>q->size;    
     q->arr = (int *)malloc(sizeof(int) * q->size);
     vector<string> order(q->size, "No Pizza");
-    
+
 
 
 
