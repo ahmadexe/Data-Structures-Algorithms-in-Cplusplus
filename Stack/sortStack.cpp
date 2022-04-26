a742e236d73ad75862ea6b63d3ba44756b00fef7
Ahmad <muahmad710@gmail.com>
2022-04-26 14:33:01 +0500

    Stack Problems

--- Stack/sortStack.cpp
+++ Stack/sortStack.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Stack{
     public:
         int top = -1;
-        vector<char> arr;
+        int* arr;
         int size;
 
         Stack(int size)
         {
             this->size = size;
+            arr = (int*)malloc(sizeof(int) * this->size);
         }
-
         int isEmpty()
         {
             if (this->top == -1)
@@ -34,7 +34,7 @@ class Stack{
             }
         }
 
-        void push(char data)
+        void push(int data)
         {
             if (this->isFull())
             {
@@ -42,29 +42,64 @@ class Stack{
             }
             else{
                 this->top++;
-                this->arr.push_back(data);
+                this->arr[this->top] = data;
             }
         }
 
-        char pop()
+        int pop()
         {
             if (!this->isEmpty()){
-                int valToReturn = arr[top];
+                int valToReturn = this->arr[this->top];
                 this->top--;
                 return valToReturn;
             }
             else{
                 cout<<"Stack underflow"<<endl;
-                return '\0';
+                return 0;
             }
         }
+
+        int topR()
+        {
+            if (!this->isEmpty())
+            {
+                return this->arr[this->top];
+            }
+            return -1;
+        }
+
 };
-Stack* s = new Stack(5);
+Stack* s = new Stack(100);
+
+void insertSorted(int val)
+{
+    if (s->arr[s->top] > val || s->isEmpty())
+    {
+        s->push(val);
+    }
+    else
+    {
+        int x = s->pop();
+        insertSorted(val);
+        s->push(x);
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
 
-    
+    insertSorted(10);
+    insertSorted(20);
+    insertSorted(5);
+    insertSorted(30);
+    insertSorted(1);
+
+    cout<<s->pop()<<endl;
+    cout<<s->pop()<<endl;
+    cout<<s->pop()<<endl;
+    cout<<s->pop()<<endl;
+    cout<<s->pop()<<endl;
 
 
     return 0;
