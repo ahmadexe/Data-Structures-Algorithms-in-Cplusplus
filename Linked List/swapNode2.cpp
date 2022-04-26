7b2c0fd2b72196a05c0a55ee13af68921d59797a
Ahmad <muahmad710@gmail.com>
2022-04-26 15:00:41 +0500

    Linked List

---
+++ Linked List/swapNode2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node{
+    int id;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+struct Node* tail = NULL;
+
+void insert(int uid){
+    if (!head && !tail){
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node*));
+        newNode->id = uid;
+        newNode->next = NULL;
+        head = newNode;
+        tail = newNode;
+    }
+    else if (head == tail) {
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node*));
+        newNode->id = uid;
+        newNode->next = NULL;
+        tail = newNode;
+        head->next = tail;
+    }
+    else {
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node*));
+        newNode->id = uid;
+        newNode->next = NULL;
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
