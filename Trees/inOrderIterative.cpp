616f384e5527fafa1a03a34be062486a0345336b
Ahmad <muahmad710@gmail.com>
2022-05-14 03:23:38 +0500

    preorder iteration

---
+++ Trees/inOrderIterative.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void insert(Node *root, int val)
+{
+    if (!root->left || !root->right)
+    {
+        Node *p = new Node(val);
+        if (p->data < root->data && !root->left)
+        {
+            root->left = p;
+            return;
+        }
+        else if (p->data > root->data && !root->right)
+        {
+            root->right = p;
+            return;
+        }
+    }
+    if (val > root->data)
+    {
+        insert(root->right, val);
+    }
+    else if (val < root->data)
+    {
+        insert(root->left, val);
+    }
+}
+
+void inOrder(Node* root)
+{
+    Node* p = root;
+    stack <Node*> s;
+    // vector<Node*> check;
+    bool done = false;
+    while (!done)
+    {
+        if (p)
+        {
+            s.push(p);
+            p = p->left;
+        }
+        else
+        {
+            if (!s.empty())
+            {
+                p = s.top();
+                s.pop();
+                cout<<p->data<<" ";
+                p = p->right;
+            }
+            else
+            {
+                done = true;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+
+    Node *root1 = new Node(40);
+    insert(root1, 30);
+    insert(root1, 10);
+    insert(root1, 15);
+    insert(root1, 50);
+    insert(root1, 60);
+    insert(root1, 70);   
+
+    inOrder(root1);
+
+    return 0;
+}
