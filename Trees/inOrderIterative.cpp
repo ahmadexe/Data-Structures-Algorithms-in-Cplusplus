7470ef3adc049150a903d6d6336c66784a7c25ae
Ahmad <muahmad710@gmail.com>
2022-05-13 23:22:26 +0500

    Finding ancestors

---
+++ Trees/inOrderIterative.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void insert(Node *root, int val)
+{
+    if (!root->left || !root->right)
+    {
+        Node *p = new Node(val);
+        if (val < root->data && !root->left)
+        {
+            root->left = p;
+            return;
+        }
+        else if (p->data > root->data && !root->right)
+        {
+            root->right = p;
+            return;
+        }
+    }
+    if (val > root->data)
+    {
+        insert(root->right, val);
+    }
+    else
+    {
+        insert(root->left, val);
+    }
+}
+
+void inOrder(Node *root)
+{
+    if (!root)
+    {
+        return;
+    }
+    else
+    {
+        inOrder(root->left);
+        cout << root->data << " ";
+        inOrder(root->right);
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
