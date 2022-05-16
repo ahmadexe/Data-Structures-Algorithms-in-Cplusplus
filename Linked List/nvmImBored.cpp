1481e8307dd2a0c4e0c85671598f834c5cfe2e45
Ahmad <muahmad710@gmail.com>
2022-05-17 03:04:52 +0500

    Help I'm bored

--- Linked List/nvmImBored.cpp
+++ Linked List/nvmImBored.cpp
@@ -11,13 +11,23 @@ struct Node{
     }
 };
 
-void insert(Node** end, int val)
+void insert(Node** start,Node** end, int val)
 {
+    Node* p = new Node(val);
+    p->next = NULL;
+
+    if (!(*end))
+    {
+        *start = p;
+        *end = p;
+    }
+    else
+    {
     Node* p = new Node(val);
     p->next = NULL;
     (*end)->next = p;
     *end = p;
-
+    }
 }
 
 void display(Node* start)
@@ -42,19 +52,19 @@ int main(int argc, char const *argv[])
     Node* tailEven = NULL;
     Node* tailOdd = NULL;
     
-    if (!head && !tail)
-    {
-        Node * p = new Node(10);
-        head = p;
-        tail = p;   
-    }
-    insert(&tail, 20);
+    // if (!head && !tail)
+    // {
+    //     Node * p = new Node(10);
+    //     head = p;
+    //     tail = p;   
+    // }
+    insert(&head,&tail, 20);
 
-    insert(&tail, 30);
+    insert(&head,&tail, 30);
 
-    insert(&tail, 40);
+    insert(&head,&tail, 40);
 
-    insert(&tail, 50);
+    insert(&head,&tail, 50);
 
  
     display(head);
