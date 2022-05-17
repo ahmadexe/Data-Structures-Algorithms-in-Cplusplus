f05cbd01836bd870bf207060799ba69d93948ad5
Ahmad <muahmad710@gmail.com>
2022-05-18 02:25:22 +0500

    Handelling edge cases

---
+++ Linked List/segregateButCooler.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *head = NULL;
+Node *tail = NULL;
+
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
