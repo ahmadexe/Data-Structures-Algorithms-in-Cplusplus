bcdb11ed5f02be7449efe249929e915e354f8a0c
Ahmad <muahmad710@gmail.com>
2022-05-18 19:58:28 +0500

    Cleaning up

--- SP21-BCS-052 Lab Mid/midsClean.cpp
+++ SP21-BCS-052 Lab Mid/midsClean.cpp
@@ -5,7 +5,6 @@ using namespace std;
 #define oneIcecream 250
 #define onePizza 1200
 
-
 struct Customer
 {
     int quantity;
@@ -120,7 +119,7 @@ void assemble()
 
             else
             {
-                Customer* q = p->customer;
+                Customer *q = p->customer;
                 while ((q->next != NULL) && q->next->quantity < toBeAdded->quantity)
                 {
                     q = q->next;
@@ -162,9 +161,9 @@ void assemble()
 int calculateTotalProfit(int idShop)
 {
     int total = 0;
-    if (idShop==1)
+    if (idShop == 1)
     {
-        Customer* q = coffee->customer;
+        Customer *q = coffee->customer;
         while (q)
         {
             total += q->quantity * oneCoffee;
@@ -173,7 +172,7 @@ int calculateTotalProfit(int idShop)
     }
     else if (idShop == 2)
     {
-        Customer* q = iceCream->customer;
+        Customer *q = iceCream->customer;
         while (q)
         {
             total += q->quantity * oneIcecream;
@@ -182,16 +181,16 @@ int calculateTotalProfit(int idShop)
     }
     else if (idShop == 3)
     {
-        Customer* q = pizza->customer;
+        Customer *q = pizza->customer;
         while (q)
         {
             total += q->quantity * onePizza;
             q = q->next;
         }
-    }   
+    }
     else
     {
-        cout<<"The id is invalid"<<endl;
+        cout << "The id is invalid" << endl;
     }
     return total;
 }
@@ -200,38 +199,38 @@ void display(int ID)
 {
     if (ID == 1)
     {
-        Customer* q = coffee->customer;
+        Customer *q = coffee->customer;
         while (q)
         {
-            cout<<"Name "<<q->name<<endl;
-            cout<<"Bought coffee in Quantity "<<q->quantity<<endl;
+            cout << "Name " << q->name << endl;
+            cout << "Bought coffee in Quantity " << q->quantity << endl;
             q = q->next;
         }
     }
 
     else if (ID == 2)
     {
-        Customer* q = iceCream->customer;
+        Customer *q = iceCream->customer;
         while (q)
         {
-            cout<<"Name "<<q->name<<endl;
-            cout<<"Bought ice cream in Quantity "<<q->quantity<<endl;
+            cout << "Name " << q->name << endl;
+            cout << "Bought ice cream in Quantity " << q->quantity << endl;
             q = q->next;
         }
     }
     else if (ID == 3)
     {
-        Customer* q = pizza->customer;
+        Customer *q = pizza->customer;
         while (q)
         {
-            cout<<"Name "<<q->name<<endl;
-            cout<<"Bought pizza in Quantity "<<q->quantity<<endl;
+            cout << "Name " << q->name << endl;
+            cout << "Bought pizza in Quantity " << q->quantity << endl;
             q = q->next;
         }
     }
     else
     {
-        cout<<"Customer with a wrong ID detected"<<endl;
+        cout << "Customer with a wrong ID detected" << endl;
     }
 }
 
@@ -239,26 +238,26 @@ void serve(int ID)
 {
     if (ID == 1)
     {
-        Customer* q = coffee->customer;
-        cout<<"Served coffee to "<<q->name<<endl;
+        Customer *q = coffee->customer;
+        cout << "Served coffee to " << q->name << endl;
         coffee->customer = coffee->customer->next;
     }
 
     else if (ID == 2)
     {
-        Customer* q = iceCream->customer;
-        cout<<"Served ice cream to "<<q->name<<endl;
+        Customer *q = iceCream->customer;
+        cout << "Served ice cream to " << q->name << endl;
         iceCream->customer = iceCream->customer->next;
     }
     else if (ID == 3)
     {
-        Customer* q = pizza->customer;
-        cout<<"Served Pizza to "<<q->name<<endl;
+        Customer *q = pizza->customer;
+        cout << "Served Pizza to " << q->name << endl;
         pizza->customer = pizza->customer->next;
     }
     else
     {
-        cout<<"Customer with a wrong ID detected"<<endl;
+        cout << "Customer with a wrong ID detected" << endl;
     }
 }
 
@@ -270,50 +269,50 @@ int main(int argc, char const *argv[])
     {
         int choice;
         cout
-        <<"1. Add customers in line"<<endl
-        <<"2. Send the customers to shops."<<endl
-        <<"3. View customers at a shop"<<endl
-        <<"4. Serve at shop"<<endl
-        <<"5. Calculate total profit"<<endl
-        <<"6. Exit"<<endl;
-        cin>>choice;
+            << "1. Add customers in line" << endl
+            << "2. Send the customers to shops." << endl
+            << "3. View customers at a shop" << endl
+            << "4. Serve at shop" << endl
+            << "5. Calculate total profit" << endl
+            << "6. Exit" << endl;
+        cin >> choice;
         if (choice == 1)
         {
-            int amount,shopid;
-            string  namePerson; 
-            cout<<"Enter qunatity: "<<endl;
-            cin>>amount;
-            cout<<"Enter the ID\n1. Coffee\n2. Ice Cream\n3. Pizza"<<endl;
-            cin>>shopid;
+            int amount, shopid;
+            string namePerson;
+            cout << "Enter qunatity: " << endl;
+            cin >> amount;
+            cout << "Enter the ID\n1. Coffee\n2. Ice Cream\n3. Pizza" << endl;
+            cin >> shopid;
             getchar();
-            cout<<"Enter the name of customer: "<<endl;
+            cout << "Enter the name of customer: " << endl;
             getline(cin, namePerson);
             addCustomer(amount, namePerson, shopid);
         }
         else if (choice == 2)
         {
             assemble();
-        }   
+        }
         else if (choice == 3)
         {
             int idView;
-            cout<<"Enter Shop ID you want to view customers of,\n1. Coffee\n2. Ice Cream\n3. Pizza"<<endl;
-            cin>>idView;
+            cout << "Enter Shop ID you want to view customers of,\n1. Coffee\n2. Ice Cream\n3. Pizza" << endl;
+            cin >> idView;
             display(idView);
         }
         else if (choice == 4)
         {
             int id;
-            cout<<"Enter Shop ID you want to serve at\n1. Coffee\n2. Ice Cream\n3. Pizza"<<endl;
-            cin>>id;
+            cout << "Enter Shop ID you want to serve at\n1. Coffee\n2. Ice Cream\n3. Pizza" << endl;
+            cin >> id;
             serve(id);
         }
         else if (choice == 5)
         {
             int id;
-            cout<<"Enter Shop ID you want to serve at\n1. Coffee\n2. Ice Cream\n3. Pizza"<<endl;
-            cin>>id;
-            cout<<calculateTotalProfit(id)<<endl;
+            cout << "Enter Shop ID you want to serve at\n1. Coffee\n2. Ice Cream\n3. Pizza" << endl;
+            cin >> id;
+            cout << calculateTotalProfit(id) << endl;
         }
         else if (choice == 6)
         {
@@ -321,10 +320,9 @@ int main(int argc, char const *argv[])
         }
         else
         {
-            cout<<"Invalid chocie"<<endl;
+            cout << "Invalid chocie" << endl;
         }
     }
-       
-    
+
     return 0;
 }
