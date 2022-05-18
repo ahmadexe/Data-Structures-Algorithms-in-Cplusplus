12276448a5783dab39703b5e2fa8bd9c18d4930f
Ahmad <muahmad710@gmail.com>
2022-05-18 16:10:45 +0500

    Cleaning up a bit

--- SP21-BCS-052 Lab Mid/midsClean.cpp
+++ SP21-BCS-052 Lab Mid/midsClean.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Customer
@@ -7,7 +7,7 @@ struct Customer
     int id;
     string name;
     int shopID;
-    Customer* next;
+    Customer *next;
     Customer(int ID, string namePerson, int shopid)
     {
         id = ID;
@@ -17,20 +17,45 @@ struct Customer
     }
 };
 
-//1 coffee, 2 icecream, 3 pizza
+// 1 coffee, 2 icecream, 3 pizza
 struct Shop
 {
     int id;
-    Shop* next;
-    Customer* customer;
-    
+    Shop *next;
+    Customer *customer;
+    Shop(int ID)
+    {
+        id = ID;
+        next = NULL;
+        customer = NULL;
+    }
 };
+
+Shop *coffee = new Shop(1);
+Shop *iceCream = new Shop(2);
+Shop *pizza = new Shop(3);
+
+Customer *headCustomer = NULL;
+Customer *tailCustomer = NULL;
+
+void form()
+{
+    coffee->next = iceCream;
+    iceCream->next = pizza;
+    pizza->next = coffee;
+}
+
+void addCustomer(int ID, string namePerson, int shopid)
+{
+    Customer* p = new Customer()
+    if (!headCustomer && !tailCustomer)
+    {
+
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-
-    
-
 
     return 0;
 }
