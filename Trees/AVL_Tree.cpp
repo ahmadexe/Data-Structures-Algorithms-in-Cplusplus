95d155a7068df8b104dbadafd651c6fa1bf08383
Ahmad <muahmad710@gmail.com>
2022-05-22 19:36:36 +0500

    Very sad me coding AVLs because that's all there's left.

--- Trees/AVL_Tree.cpp
+++ Trees/AVL_Tree.cpp
@@ -10,6 +10,8 @@ struct Node
     Node(int val)
     {
         data = val;
+        left = NULL;
+        right = NULL;
     }
 };
 
@@ -39,6 +41,7 @@ Node* rightRotation(Node* root)
         Node* temp = x->right;
         x->right = root;
         root->left = temp;
+        return x;
     }
 }
 
@@ -54,14 +57,84 @@ Node* leftRotation(Node* root)
         Node* temp = x->left;
         x->left = root;
         root->right = temp;
+        return x;
     }
+}
+
+int height(Node* root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+    return (max(height(root->left), height(root->right)) + 1);
+}
+
+int balanceFactor(Node* root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+    int left = height(root->left);
+    int right = height(root->right);
+    return (left - right);
+}
+
+Node* insert(Node* r, Node* insertNode)
+{
+    if (!r)
+    {
+        r = insertNode;
+    }
+    else if (insertNode->data > r->data)
+    {
+        r->right = insert(r->right, insertNode);
+    }
+    else if (insertNode->data < r->data)
+    {
+        r->left = insert(r->left, insertNode);
+    }
+    else
+    {
+        cout<<"Can't add duplicate values"<<endl;
+        return r;
+    }
+    
+    int bf = balanceFactor(r);
+
+    if (bf > 1 && r->left->data > insertNode->data)
+    {
+        return rightRotation(r);
+    }
+    if (bf > 1 && r->left->data < insertNode->data)
+    {
+        r->left = leftRotation(r);
+        return rightRotation(r);
+    }
+    if (bf < -1 && r->right->data < insertNode->data)
+    {
+        return leftRotation(r);
+    }
+    if (bf < -1 && r->right->data > insertNode->data)
+    {
+        r->right = rightRotation(r);
+        return leftRotation(r);
+    }
+    return r;
 }
 
 int main(int argc, char const *argv[])
 {
 
-    
+    Node* root = new Node(50);   
+    root = insert(root, new Node(40));
+    root = insert(root, new Node(30));
+    root = insert(root, new Node(20));
+    root = insert(root, new Node(10));
 
-
+    inOrder(root);
+ 
+ 
     return 0;
 }
