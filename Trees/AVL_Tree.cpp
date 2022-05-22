a9c2347ab25849e58c5faebfabea276818ca2053
Ahmad <muahmad710@gmail.com>
2022-05-22 19:18:01 +0500

    Very sad me coding AVLs because that's all there's left.

--- Trees/AVL_Tree.cpp
+++ Trees/AVL_Tree.cpp
@@ -13,7 +13,49 @@ struct Node
     }
 };
 
+void inOrder(Node* root)
+{
+    if (!root)
+    {
+        return;
+    }
+    else
+    {
+        inOrder(root->left);
+        cout<<root->data;
+        inOrder(root->right);
+    }
+}
 
+Node* rightRotation(Node* root)
+{
+    if (!root->left)
+    {
+        return root;
+    }
+    else
+    {
+        Node* x = root->left;
+        Node* temp = x->right;
+        x->right = root;
+        root->left = temp;
+    }
+}
+
+Node* leftRotation(Node* root)
+{
+    if (!root->right)
+    {
+        return root;
+    }
+    else
+    {
+        Node* x = root->right;
+        Node* temp = x->left;
+        x->left = root;
+        root->right = temp;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
