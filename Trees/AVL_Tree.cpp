b21636403ffe60bb9da07bc5033ebca41fb1c633
Ahmad <muahmad710@gmail.com>
2022-05-22 19:14:05 +0500

    Very sad me coding AVLs because that's all there's left.

---
+++ Trees/AVL_Tree.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        data = val;
+    }
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
