9f380bdf3b8fc7dbf44038fa9646090facf3cd0b
Ahmad <muahmad710@gmail.com>
2022-05-24 09:35:42 +0500

    Min Heap implementation

--- Trees/min_heap.cpp
+++ Trees/min_heap.cpp
@@ -15,7 +15,7 @@ struct Heap
     }
 };
 
-Heap *heap = new Heap(10);
+Heap *heap = new Heap(20);
 
 int isFull()
 {
@@ -35,15 +35,12 @@ void insert(int val)
     while (i != 0 && parentData > heap->arr[i])
     {
         int parentIndex = (i - 1) / 2;
-        if (heap->arr[parentIndex] > heap->arr[i])
+        parentData = heap->arr[parentIndex];
+        if (parentData > heap->arr[i])
         {
             int temp = heap->arr[i];
             heap->arr[i] = heap->arr[parentIndex];
             heap->arr[parentIndex] = temp;
-        }
-        else
-        {
-            break;
         }
         i = parentIndex;
     }
@@ -60,11 +57,20 @@ void print()
 
 int main(int argc, char const *argv[])
 {
-    insert(10);
-    insert(20);
-    insert(1);
     insert(3);
-    insert(2);
+    insert(9);
+    insert(5);
+    insert(37);
+    insert(16);
+    insert(12);
+    insert(22);
+    insert(4);
+    insert(44);
+    insert(32);
+    insert(19);
+    insert(47);
+    insert(50);
+
     print();
     
     return 0;
