bac5534a421deb6a92b7f951a6198f38b909fafb
Ahmad <muahmad710@gmail.com>
2022-05-27 03:18:07 +0500

    Depth First Search

--- Graphs/DepthFirstSearch.cpp
+++ Graphs/DepthFirstSearch.cpp
@@ -6,6 +6,30 @@ const int N = 1e3;
 vector<int> vertices[N];
 bool visited[N];
 
+void dfs(int vertex)
+{
+    /*
+    * Take action after entering the vertex.
+    */
+    if (visited[vertex])
+        return;
+    visited[vertex] = true;
+    cout<<vertex<<" ";
+    for (int child : vertices[vertex])
+    {
+    /*
+    * Take action on child vertex before it sends its child on top of the stack
+    */
+        dfs(child);
+    /*
+    * Take action on the child after leaving the child.
+    */
+    }
+    /*
+    * Take action on the vertex before leaving the vertex.
+    */
+}
+
 int main(int argc, char const *argv[])
 {
     int nodes, edges;
@@ -17,6 +41,8 @@ int main(int argc, char const *argv[])
         vertices[v1].push_back(v2);
         vertices[v2].push_back(v1);
     }
+
+    dfs(vertices[1][0]);
 
     return 0;
 }
