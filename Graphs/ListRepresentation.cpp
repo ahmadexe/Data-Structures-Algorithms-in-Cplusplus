735b781d5cbcfc947fd1153f7fd33e964cddaae5
Ahmad <muahmad710@gmail.com>
2022-05-26 23:45:30 +0500

    Adjency List Representation of Graphs

--- Graphs/ListRepresentation.cpp
+++ Graphs/ListRepresentation.cpp
@@ -1,19 +1,34 @@
+// Representation with weights
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> v[10];
 
 int main(int argc, char const *argv[])
 {
-    vector<int> b = {1,2,3,4};
-    vector<int>* v = &b;
-    v[0].push_back(10);
-    cout<<v[0][0];
+    v[0].push_back(0);
+    v[0].push_back(1);
+    v[0].push_back(2);
+    v[0].push_back(3);
+    v[0].push_back(4);
 
+    v[1].push_back(10);
+    v[1].push_back(20);
+    v[1].push_back(30);
+    v[1].push_back(40);
+    v[1].push_back(50);
 
-    
-    
+    v[1];
 
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 
     return 0;
