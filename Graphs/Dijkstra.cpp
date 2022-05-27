2b4d838320f87fb5aa53128a646d413a7d0ba402
Ahmad <muahmad710@gmail.com>
2022-05-28 02:55:56 +0500

    Dijkstra Algorithm - Shortest Path in +vely weighted graph (For directed Graph)

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -6,10 +6,30 @@ const int N = 1e5+10;
 const int INF = 1e7+10;
 vector<pair<int,int>> graph[N];
 
-void dijkstra(int source)
+vector<int> dijkstra(int source)
 {
     vector<int> distance(N, INF);
-    
+    set<pair<int,int>> s;
+    distance[source] = 0;
+    s.insert({0, source});
+    while (!s.empty())
+    {
+        auto current = *(s.begin());
+        int weightCurrent = current.first;
+        int vertex = current.second;
+        s.erase(s.begin());
+        for (auto child : graph[vertex])
+        {
+            int childVertex = child.first;
+            int childWeight = child.second;
+            if (childWeight + weightCurrent < distance[childVertex])
+            {
+                distance[childVertex] = childWeight + weightCurrent;
+                s.insert({distance[childVertex], childVertex});
+            }
+        } 
+    }
+    return distance;
 }
 
 int main(int argc, char const *argv[])
@@ -20,11 +40,16 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < edges; i++)
     {
         int v1,v2,wt;
-        cin>>v1,v2,wt;
+        cin>>v1>>v2>>wt;
         graph[v1].push_back({v2, wt});
     }
 
-    dijkstra(0);
+    vector<int> distance = dijkstra(0);
+     for (int i = 0; i < vertices; i++)
+    {
+        cout<<"0"<<" - "<<i<<" "<<distance[i]<<endl;
+    }
+    
 
     return 0;
 }
