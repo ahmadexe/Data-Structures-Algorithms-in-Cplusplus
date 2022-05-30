19ab1df9bb181019810f943bb078659edeee78b6
Ahmad <muahmad710@gmail.com>
2022-05-31 01:44:06 +0500

    Adjency List - Problem geeks for geeks

--- Graphs/Adjency.cpp
+++ Graphs/Adjency.cpp
@@ -4,18 +4,36 @@ using namespace std;
 const int N = 1e3;
 static int arr[N][N];
 
+void printList(int vertexTotal)
+{
+    for (int i = 0; i < vertexTotal; i++)
+    {
+        cout<<i;
+        for (int j = 0; j < vertexTotal; j++)
+        {
+            if (arr[i][j])
+            {
+                cout<<"-> "<<j;
+            }
+        }
+        cout<<endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 
-    int edges;
-    cin>>edges;
+    int vertex, edges;
+    cin>>vertex>>edges;
     for (int i = 0; i < edges; i++)
     {
         int v1,v2;
+        cin>>v1>>v2;
         arr[v1][v2] = 1;
         arr[v2][v1] = 1;
     }
 
+    printList(vertex);
 
     return 0;
 }
