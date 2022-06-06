1589851a00e2d539a1a20e81d2fd17b9ea513a9d
Ahmad <muahmad710@gmail.com>
2022-06-07 03:53:10 +0500

    Prim's Added

--- Graphs/Prims.cpp
+++ Graphs/Prims.cpp
@@ -2,12 +2,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5+10;
+const int INF = 1e7+10;
+vector<pair<int,int>> graph[N];
+vector<int> distanceArr(N, INF);
+vector<int> parent(N, -1);
+bool visited[N];
+
+void Prims(int source)
+{
+    set<pair<int,int>> s;
+    distanceArr[source] = 0;
+    s.insert({0, source});
+    while (!s.empty())
+    {
+        auto current = *(s.begin());
+        int weightCurrent = current.first;
+        int vertex = current.second;
+        s.erase(s.begin());
+        visited[vertex] = true;
+        for (auto child : graph[vertex])
+        {
+            int childVertex = child.first;
+            int childWeight = child.second;
+            if (childWeight < distanceArr[childVertex] && !visited[childVertex])
+            {
+                distanceArr[childVertex] = childWeight;
+                parent[childVertex] = vertex;
+                s.insert({distanceArr[childVertex], childVertex});
+            }
+        } 
+    }
+}
 
 int main(int argc, char const *argv[])
 {
 
-    
+    int vertices, edges;
+    cin>>vertices>>edges;
+    for (int i = 0; i < edges; i++)
+    {
+        int v1,v2,wt;
+        cin>>v1>>v2>>wt;
+        graph[v1].push_back({v2, wt});
+        graph[v2].push_back({v1, wt});
+    }
 
+    Prims(0);
+    int sum = 0;
+    for (int i = 0; i <= vertices; i++)
+    {
+        sum += distanceArr[i];
+    }
+    cout<<sum<<endl;
 
     return 0;
 }
