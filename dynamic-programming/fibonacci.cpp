26ebf17923c4937d7f1971c7c4e2e7183d000276
ahmadexe <muahmad710@gmail.com>
2023-02-06 12:34:01 +0500

    minor changes

--- dynamic-programming/fibonacci.cpp
+++ dynamic-programming/fibonacci.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void fib(int a, int b) {
-    if (b > 50) return;
+void fib(int a, int b, int n) {
+    if (b > n) return;
     int c = a + b;
     cout << c << endl;
-    fib(b, c);
+    fib(b, c, n);
 }
 
 int getNthFibElement(int n) {
     if (n == 1) return 0;
-    if (n == 2) return 1;
+    else if (n == 2) return 1;
     return getNthFibElement(n - 1) + getNthFibElement(n - 2);
 }
 
 int main(int argc, char const *argv[])
 {
-    fib(0, 1);
+    fib(0, 1, 50);
     cout << getNthFibElement(6) << endl;
     return 0;
 }
