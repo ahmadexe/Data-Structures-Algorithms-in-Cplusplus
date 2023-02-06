8bec0f73900d8d833a1105cecfb287ec39bf095c
ahmadexe <muahmad710@gmail.com>
2023-02-06 12:39:16 +0500

    added memoization to findind nth fib element

--- dynamic-programming/fibonacci.cpp
+++ dynamic-programming/fibonacci.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <unordered_map> 
 using namespace std;
+
+#define mod 100000047
 
 void fib(int a, int b, int n) {
     if (b > n) return;
@@ -14,9 +17,19 @@ int getNthFibElement(int n) {
     return getNthFibElement(n - 1) + getNthFibElement(n - 2);
 }
 
+int memoizedSearched(int n) {
+    static unordered_map<int, int> memo;
+    if (memo.find(n) != memo.end()) return memo[n];
+    if (n == 1) return 0;
+    else if (n == 2) return 1;
+    memo[n] = memoizedSearched(n - 1) + memoizedSearched(n - 2);
+    return memo[n] % mod;
+}
+
 int main(int argc, char const *argv[])
 {
-    fib(0, 1, 50);
-    cout << getNthFibElement(6) << endl;
+    // fib(0, 1, 50);
+    // cout << getNthFibElement(6) << endl;
+    cout << memoizedSearched(50) << endl;
     return 0;
 }
